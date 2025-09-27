ab2833523df43abfa277ca69df30d274a261df97
Kaitlynn Oxley <97kaity@gmail.com>
2025-09-27 13:16:36 -0700

    Header

--- vite-project/src/Header/Header.jsx
+++ vite-project/src/Header/Header.jsx
@@ -1,5 +1,19 @@
+import "../Header/Header.css";
+import headerLogo from "../assets/logo.svg";
+import avatar from "../assets/avatar.svg";
+
 function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header className="header">
+      <img src={headerLogo} className="header__logo" />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-button">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
